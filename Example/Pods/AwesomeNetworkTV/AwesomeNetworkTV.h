4191b79756534d10d49ea1e8547c91a892886842
Evandro Harrison Hoffmann <evandro@itsdayoff.com>
2019-03-10 09:33:08 +0800

    Added TVFramework

---
+++ Example/Pods/AwesomeNetworkTV/AwesomeNetworkTV.h
@@ -0,0 +1,18 @@
+//
+//  AwesomeNetworkTV.h
+//  AwesomeNetworkTV
+//
+//  Created by Evandro Harrison on 10/03/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AwesomeNetworkTV.
+FOUNDATION_EXPORT double AwesomeNetworkTVVersionNumber;
+
+//! Project version string for AwesomeNetworkTV.
+FOUNDATION_EXPORT const unsigned char AwesomeNetworkTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeNetworkTV/PublicHeader.h>
+
+
